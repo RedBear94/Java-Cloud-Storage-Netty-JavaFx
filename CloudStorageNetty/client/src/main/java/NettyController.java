45aef4713765e99b0939a0988783497e7051eccf
Pavel <redbear95@gmail.com>
2020-08-05 20:14:38 +0300

    HomeWork3

---
+++ CloudStorageNetty/client/src/main/java/NettyController.java
@@ -0,0 +1,11 @@
+import javafx.event.ActionEvent;
+
+public class NettyController {
+    public void sendCommand(ActionEvent actionEvent) {
+
+    }
+
+    public void exitAction(ActionEvent actionEvent) {
+
+    }
+}
